de6af37f3a627b64db33df19c4580e4f836e66ba
Kayle54187 <habichristi@gmail.com>
2025-01-14 23:20:08 +0200

    feat: learning futures

---
+++ futures_simple_test/bin/futures_simple_test.dart
@@ -0,0 +1,13 @@
+import "dart:async";
+
+void main(List<String> arguments) async {
+  Future<String> apiData() async {
+    Future.delayed(Duration(seconds: 2));
+    return "Hello World";
+  }
+
+  print("Data Loading...");
+  String data = await apiData();
+  print("Api Data: $data");
+  print("Data Loaded");
+}
